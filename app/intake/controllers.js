cd3359c0dc5051926f0af543bdd34a545c0a9e3b
Shih-Wen Su <sushi@summeridea.com>
2016-10-22 15:09:00 -0500

    add d3.js

--- app/intake/controllers.js
+++ app/intake/controllers.js
@@ -30,7 +30,121 @@ angular.module('intake.controllers', [])
     DataStore.get('api/customers', $state.params._id)
     .then(function(client) {
       $scope.client = client;
-    })
+    });
+    $scope.checkinCount = 26;
+    $scope.checkinSignTop = 12;
+    $scope.checkIn = function() {
+      $scope.checkinCount++;
+      $scope.checkedin = true;
+      $scope.checkinSignTop = 10;
+    };
+
+    var showSparkline = function() {
+      // create an SVG element inside the #graph div that fills 100% of the div
+      var graph = d3.select("#graph").append("svg:svg").attr("width", "100%").attr("height", "100%");
+
+      // create a simple data array that we'll plot with a line (this array represents only the Y values, X will just be the index location)
+      var data = [3, 6, 2, 7, 5, 2, 1, 3, 8, 9, 2, 5, 9, 3, 6, 3, 6, 2, 7, 5, 2, 1, 3, 8, 9, 2, 5, 9, 2, 7, 5, 2, 1, 3, 8, 9, 2, 5, 9, 3, 6, 2, 7, 5, 2, 1, 3, 8, 9, 2, 5, 9];
+
+      // X scale will fit values from 0-10 within pixels 0-100
+      var x = d3.scale.linear().domain([0, 10]).range([0, 50]);
+      // Y scale will fit values from 0-10 within pixels 0-100
+      var y = d3.scale.linear().domain([0, 10]).range([0, 30]);
+
+      // create a line object that represents the SVN line we're creating
+      var line = d3.svg.line()
+        // assign the X function to plot our line as we wish
+        .x(function(d,i) { 
+          // verbose logging to show what's actually being done
+          console.log('Plotting X value for data point: ' + d + ' using index: ' + i + ' to be at: ' + x(i) + ' using our xScale.');
+          // return the X coordinate where we want to plot this datapoint
+          return x(i); 
+        })
+        .y(function(d) { 
+          // verbose logging to show what's actually being done
+          console.log('Plotting Y value for data point: ' + d + ' to be at: ' + y(d) + " using our yScale.");
+          // return the Y coordinate where we want to plot this datapoint
+          return y(d); 
+        })
+  
+      // display the line by appending an svg:path element with the data line we created above
+      graph.append("svg:path").attr("d", line(data));
+    };
+    showSparkline();
+
+    var callD3Calendar = function() {
+      var width = 260,
+          height = 136,
+          cellSize = 17; // cell size
+
+      var percent = d3.format(".1%"),
+          format = d3.time.format("%Y-%m-%d");
+
+      var color = d3.scale.quantize()
+          .domain([-.05, .05])
+          .range(d3.range(11).map(function(d) { return "q" + d + "-11"; }));
+
+      var svg = d3.select("#calendar").selectAll("svg")
+          .data(d3.range(2014, 2015))
+        .enter().append("svg")
+          .attr("width", width)
+          .attr("height", height)
+          .attr("class", "RdYlGn")
+        .append("g")
+          .attr("transform", "translate(" + ((width - cellSize * 53) / 2) + "," + (height - cellSize * 7 - 1) + ")");
+
+      svg.append("text")
+          .attr("transform", "translate(-6," + cellSize * 3.5 + ")rotate(-90)")
+          .style("text-anchor", "middle")
+          .text(function(d) { return d; });
+
+      var rect = svg.selectAll(".day")
+          .data(function(d) { return d3.time.days(new Date(d, 0, 1), new Date(d + 1, 0, 1)); })
+        .enter().append("rect")
+          .attr("class", "day")
+          .attr("width", cellSize)
+          .attr("height", cellSize)
+          .attr("x", function(d) { return d3.time.weekOfYear(d) * cellSize; })
+          .attr("y", function(d) { return d.getDay() * cellSize; })
+          .datum(format);
+
+      rect.append("title")
+          .text(function(d) { return d; });
+
+      svg.selectAll(".month")
+          .data(function(d) { return d3.time.months(new Date(d, 0, 1), new Date(d + 1, 0, 1)); })
+        .enter().append("path")
+          .attr("class", "month")
+          .attr("d", monthPath);
+
+      d3.csv("dji.csv", function(csv) {
+        console.log(csv);
+        // console.dir(error);
+        // if (error) throw error;
+
+        var data = d3.nest()
+          .key(function(d) { return d.Date; })
+          .rollup(function(d) { return (d[0].Close - d[0].Open) / d[0].Open; })
+          .map(csv);
+
+        rect.filter(function(d) { return d in data; })
+            .attr("class", function(d) { return "day " + color(data[d]); })
+          .select("title")
+            .text(function(d) { return d + ": " + percent(data[d]); });
+      });
+
+      function monthPath(t0) {
+        var t1 = new Date(t0.getFullYear(), t0.getMonth() + 1, 0),
+            d0 = t0.getDay(), w0 = d3.time.weekOfYear(t0),
+            d1 = t1.getDay(), w1 = d3.time.weekOfYear(t1);
+        return "M" + (w0 + 1) * cellSize + "," + d0 * cellSize
+            + "H" + w0 * cellSize + "V" + 7 * cellSize
+            + "H" + w1 * cellSize + "V" + (d1 + 1) * cellSize
+            + "H" + (w1 + 1) * cellSize + "V" + 0
+            + "H" + (w0 + 1) * cellSize + "Z";
+      }
+    };
+    $timeout(callD3Calendar());
   })
 //Client List Deprecated
 // .controller('CheckClientListController',
