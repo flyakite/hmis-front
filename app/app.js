f23d462992491a969b78b55d58196698506e7e88
Shih-Wen Su <sushi@summeridea.com>
2016-10-22 22:01:47 -0500

    update

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ui.router',
+  'ui.bootstrap',
   'ngRoute',
   'app.services',
   'intake.controllers'
